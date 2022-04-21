25f8b5ea8ef0a9767beea68fd4300143fb293e03
ElsaRimhagen <elsa.rimhagen@gmail.com>
2022-04-21 10:50:12 +0200

    lagt till block

--- screens/MotionMenu.tsx
+++ screens/MotionMenu.tsx
@@ -15,10 +15,21 @@ const MotionMenu = () => {
     return (
       <View style={styles.scrollContainer}>
         <ScrollView>
+          
           <ClickableBlock source = {require("../assets/move_10steps.png")} />
           <ClickableBlock source={require("../assets/turn_right_15deg.png")} />
           <ClickableBlock source={require("../assets/turn_left_15deg.png")} />
-          <ClickableBlock source = {require("../assets/move_10steps.png")} />
+          <ClickableBlock source = {require("../assets/go_to_random.png")} />
+          <ClickableBlock source = {require("../assets/goto_x_y.png")} />
+          <ClickableBlock source = {require("../assets/glide_secs_random.png")} />
+          <ClickableBlock source = {require("../assets/glide_secs_x_y.png")} />
+          <ClickableBlock source = {require("../assets/point_direction_90.png")} />
+          <ClickableBlock source = {require("../assets/point_mouse_pointer.png")} />
+          <ClickableBlock source = {require("../assets/change_x_10.png")} />
+          <ClickableBlock source = {require("../assets/set_x_to_0.png")} />
+          <ClickableBlock source = {require("../assets/change_y_10.png")} />
+          <ClickableBlock source = {require("../assets/set_y_0.png")} />
+          
         </ScrollView>
       </View>
     );
