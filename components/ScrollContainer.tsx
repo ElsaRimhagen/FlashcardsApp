02a2a498d299f67ede43c19d7fb84214fd985d7a
ElsaRimhagen <elsa.rimhagen@gmail.com>
2022-05-04 15:02:36 +0200

    småfix

--- components/ScrollContainer.tsx
+++ components/ScrollContainer.tsx
@@ -23,8 +23,8 @@ export const ScrollContainer = ({ chosenCategory }: Props) => {
   const renderClickableBlock = ({ item }: { item: any }) => {
     return (
       <ClickableBlock
-        source={item.source}
-        onPress={() => {
+        source = {item.source}
+        onPress = {() => {
           navigation.navigate("FlashcardView", {
             frontside_block: item.source,
             translation: item.translation,
