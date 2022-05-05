fdfc118ddfcf8994bd141f32ea5e6fc56fdeb527
danielemanuelsson <daniel.emanuelsson@precisit.com>
2022-05-05 11:15:42 +0200

    Lagt styles i bokstavsordning

--- styles.ts
+++ styles.ts
@@ -14,13 +14,6 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-  littleblueContainer: {
-    width: "100%",
-    height: "5%",
-    backgroundColor: colors.background,
-    alignItems: "center",
-    justifyContent: "center",
-  },
   box: {
     width: "50%",
     height: "25%",
@@ -45,8 +38,24 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  flashcardContainer: {
+    height: "50%",
+    backgroundColor: colors.background,
+    padding: 5,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    alignContent: "center",
+    justifyContent: "center",
+  },
   innerbox: {
     flex: 1,
+    backgroundColor: colors.background,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  littleblueContainer: {
+    width: "100%",
+    height: "5%",
     backgroundColor: colors.background,
     alignItems: "center",
     justifyContent: "center",
@@ -59,15 +68,6 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-  flashcardContainer: {
-    height: "50%",
-    backgroundColor: colors.background,
-    padding: 5,
-    flexDirection: "row",
-    flexWrap: "wrap",
-    alignContent: "center",
-    justifyContent: "center",
-  },
   translationContainerStyle: {
     width: 270,
     height: 100,
@@ -78,10 +78,20 @@ const styles = StyleSheet.create({
   },
 
   // Headers & Text //
+  bottomText: {
+    marginTop: -25,
+    marginBottom: 15,
+  },
   categorytextStyle: {
     color: colors.headers,
     fontFamily: "American-Typewriter",
     fontSize: 30,
+  },
+  constructorTextStyle: {
+    color: colors.headers,
+    fontFamily: "American-Typewriter",
+    fontSize: 40,
+    textAlign: "center",
   },
   headerStyle: {
     width: "100%",
@@ -90,20 +100,10 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     marginTop: 15,
   },
-  bottomText: {
-    marginTop: -25,
-    marginBottom: 15,
-  },
   textStyle: {
     color: colors.text,
     fontFamily: "American-Typewriter",
     fontSize: 30,
-    textAlign: "center",
-  },
-  constructorTextStyle: {
-    color: colors.headers,
-    fontFamily: "American-Typewriter",
-    fontSize: 40,
     textAlign: "center",
   },
   titleStyle: {
@@ -118,16 +118,16 @@ const styles = StyleSheet.create({
   },
 
   // Images & logotypes //
-  blockStyle: {
-    height: 60,
+  bigBlockStyle: {
+    height: 65,
     resizeMode: "contain",
     padding: 10,
     margin: 10,
     alignItems: "center",
     justifyContent: "center",
   },
-  bigBlockStyle: {
-    height: 65,
+  blockStyle: {
+    height: 60,
     resizeMode: "contain",
     padding: 10,
     margin: 10,
@@ -149,28 +149,20 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-  haveFunStyle: {
-    height: 100,
-    resizeMode: "contain",
-    padding: 10,
-    margin: 10,
+  flashcardBacksideStyle: {
+    backgroundColor: colors.flashcard,
+    position: "absolute",
+    top: 0,
+  },
+  flashcardStyle: {
+    width: 350,
+    height: 250,
+    backgroundColor: colors.flashcard,
     alignItems: "center",
     justifyContent: "center",
-  },
-  GoBackStyle: {
-    position: "absolute",
-    left: 5,
-    top: 15,
-    resizeMode: "contain",
-    width: 60,
-    height: 60,
-  },
-  GoBackTouchableOpacity: {
-    position: "absolute",
-    width: 60,
-    height: 60,
-    left: 5,
-    top: 15,
+    backfaceVisibility: "hidden",
+    borderWidth: 4,
+    borderColor: colors.text,
   },
   GoForwardStyle: {
     position: "absolute",
@@ -187,28 +179,34 @@ const styles = StyleSheet.create({
     right: 5,
     bottom: 15,
   },
-  
+  GoBackStyle: {
+    position: "absolute",
+    left: 5,
+    top: 15,
+    resizeMode: "contain",
+    width: 60,
+    height: 60,
+  },
+  GoBackTouchableOpacity: {
+    position: "absolute",
+    width: 60,
+    height: 60,
+    left: 5,
+    top: 15,
+  },
+  haveFunStyle: {
+    height: 100,
+    resizeMode: "contain",
+    padding: 10,
+    margin: 10,
+    alignItems: "center",
+    justifyContent: "center",
+  },
   imgStyle: {
     width: 80,
     height: 80,
     resizeMode: "stretch",
   },
-  flashcardStyle: {
-    width: 350,
-    height: 250,
-    backgroundColor: colors.flashcard,
-    alignItems: "center",
-    justifyContent: "center",
-    backfaceVisibility: "hidden",
-    borderWidth: 4,
-    borderColor: colors.text,
-  },
-  flashcardBacksideStyle: {
-    backgroundColor: colors.flashcard,
-    position: "absolute",
-    top: 0,
-  },
-  
 });
 
 export default styles;
